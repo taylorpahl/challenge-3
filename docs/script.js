6d44eea6133c56ef4e66a869c892436bd1953b2d
Taylor P <taylorpahl97@gmail.com>
2022-11-09 17:37:21 -0600

    Added validation response for input

--- docs/script.js
+++ docs/script.js
@@ -6,23 +6,67 @@ function generatePassword() {
 // 1) Prompt user for password criteria
   // 1-a. Password length 8 - 128
 var passwordLength;
+var useUppercase;
+var useLowercase;
+var useNumbers;
+var useSpecial;
+var charSets;
 passwordLength = window.prompt("How many characters in password?");
 // 2) Validate the input
-
+if (!passwordLength) {
+  alert("Input required");
+  return;
+}
+if (passwordLength === null) {
+  window.alert("Input required");
+  return;
+}
+if (isNaN(passwordLength)) {
+  window.alert("Must input a number between 8 and 128.");
+  return;
+}
+if (passwordLength < 8 || passwordLength > 128) {
+  window.alert("Must input a number between 8 and 128.");
+  return;
+}
   // 1-b. Uppercase, Lowercase, Numbers, Speacial Characters?
-window.confirm("Do you want uppercase letters in password?");
-// 2) Validate the input
-window.confirm("Do you want lowercase letters in password?");
-// 2) Validate the input
-window.confirm("Do you want numbers in password?");
-// 2) Validate the input
-window.confirm("Do you want special characters in password?");
-// 2) Validate the input
+  else {
+    useUppercase = window.confirm("Do you want uppercase letters in password?");
+    // 2) Validate the input
+    if (!useUppercase) {
+      charSets = "abcdefghijklmnopqrstuvwxyz0123456789!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+    } else {
+      charSets = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+    }
+  }
 
+    useLowercase.window.confirm("Do you want lowercase letters in password?");
+    // 2) Validate the input
+    if (!useLowercase) {
+      charSets = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+    } else {
+      charSets = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+    }
+  
+    useNumbers.window.confirm("Do you want numbers in password?");
+    // 2) Validate the input
+    if (!useNumbers) {
+      charSets = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+    } else {
+      charSets = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+    }
+
+    window.confirm("Do you want special characters in password?");
+    // 2) Validate the input
+    if (!useSpecial) {
+      charSets = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    } else {
+      charSets = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+    }
 // 3) Generate password
 
 // 4) Display password on screen
-  return "Generated password";
+  return;
 }
 
 // Write password to the #password input
