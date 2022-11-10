49354d4d3b49c2d5e7bbed1bc840208e197342d6
Taylor P <taylorpahl97@gmail.com>
2022-11-10 16:24:56 -0600

    Changed confirm messages

--- docs/script.js
+++ docs/script.js
@@ -8,67 +8,42 @@ var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
 var special = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"];
 
 function generatePassword() {
-  
-// 1) Prompt user for password criteria
-  // 1-a. Password length 8 - 128
+
 var passwordLength;
 var useUppercase;
 var useLowercase;
 var useNumbers;
 var useSpecial;
 var charSets;
+
+// 1) Prompt user for password criteria
+
+// 1-a. Password length 8 - 128
 passwordLength = window.prompt("How many characters in password?");
+
 // 2) Validate the input
 if (!passwordLength) {
   window.alert("Input required");
   return;
 }
-if (passwordLength === null) {
-  window.alert("Input required");
-  return;
-}
-if (isNaN(passwordLength)) {
+else if (isNaN(passwordLength)) {
   window.alert("Must input a number between 8 and 128.");
   return;
 }
-if (passwordLength < 8 || passwordLength > 128) {
+else if (passwordLength < 8 || passwordLength > 128) {
   window.alert("Must input a number between 8 and 128.");
   return;
 }
-  // 1-b. Uppercase, Lowercase, Numbers, Speacial Characters?
-  else {
-    useUppercase = window.confirm("Do you want uppercase letters in password?");
-    // 2) Validate the input
-    if (!useUppercase) {
-      charSets = "abcdefghijklmnopqrstuvwxyz0123456789!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
-    } else {
-      charSets = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
-    }
-  }
+// 1-b. Uppercase, Lowercase, Numbers, Speacial Characters?
+else { 
+  useUppercase = window.confirm("Do you want uppercase letters in password?");
+  useLowercase = window.confirm("Do you want lowercase letters in password?");
+  useNumbers = window.confirm("Do you want numbers in password?");
+  useSpecial = window.confirm("Do you want special characters in password?");
+}
 
-    useLowercase.window.confirm("Do you want lowercase letters in password?");
-    // 2) Validate the input
-    if (!useLowercase) {
-      charSets = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
-    } else {
-      charSets = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
-    }
-  
-    useNumbers.window.confirm("Do you want numbers in password?");
-    // 2) Validate the input
-    if (!useNumbers) {
-      charSets = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
-    } else {
-      charSets = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
-    }
+// 2) Validate the input
 
-    window.confirm("Do you want special characters in password?");
-    // 2) Validate the input
-    if (!useSpecial) {
-      charSets = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    } else {
-      charSets = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
-    }
 // 3) Generate password
 
 // 4) Display password on screen
