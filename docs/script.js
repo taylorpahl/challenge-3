7a549bb701c4670823307d27abe42c1910866434
Taylor P <taylorpahl97@gmail.com>
2022-11-10 15:57:03 -0600

    Added character set arrays

--- docs/script.js
+++ docs/script.js
@@ -1,6 +1,12 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Character set arrays
+var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+var special = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"];
+
 function generatePassword() {
   
 // 1) Prompt user for password criteria
@@ -14,7 +20,7 @@ var charSets;
 passwordLength = window.prompt("How many characters in password?");
 // 2) Validate the input
 if (!passwordLength) {
-  alert("Input required");
+  window.alert("Input required");
   return;
 }
 if (passwordLength === null) {
